cc9edc3e53aa950a9efbd54c2a5f9553f6854718
James Simpson <james.simpson9339@gmail.com>
2017-10-31 00:47:26 -0400

    add extlib 3rdparty and third_party as the list of external library folders

--- cmake/scaffold/sf_extlib.cmake
+++ cmake/scaffold/sf_extlib.cmake
@@ -3,18 +3,21 @@
 include("${CMAKE_CURRENT_LIST_DIR}/sf_include.cmake")
 sf_include_sf_dependency_once(sf_log)
 
-# looks for libray folder `extlib` or`3rdparty` in paths
+# looks for libray folder `extlibs` `3rdparty` or `third_party` in paths
 # starting with `extlib` or `3rdparty`
 macro(sf_find_extlib_in extlib dir)
     sf_message("finding ${extlib} in ${dir}")
 
     list(APPEND CMAKE_MODULE_PATH
         "${CMAKE_SOURCE_DIR}/${dir}/cmake/modules"
-        "${CMAKE_SOURCE_DIR}/${dir}/cmake"
-        "${CMAKE_SOURCE_DIR}/extlibs/cmake/modules"
-        "${CMAKE_SOURCE_DIR}/extlibs/cmake"
-        "${CMAKE_SOURCE_DIR}/3rdparty/cmake/modules"
-        "${CMAKE_SOURCE_DIR}/3rdparty/cmake")
+        "${CMAKE_SOURCE_DIR}/${dir}/cmake")
+
+    list(APPEND ext_lib_list "exlibs" "3rdparty" "third_party")
+    foreach(list_item ${ext_lib_list})
+        list(APPEND CMAKE_MODULE_PATH
+            "${CMAKE_SOURCE_DIR}/${list_item}/cmake"
+            "${CMAKE_SOURCE_DIR}/${list_item}/cmake/modules")
+    endforeach()
 
     find_package("${extlib}" REQUIRED)
     string(TOUPPER "${extlib}" "${extlib}_UPPER")
