f6ca504b51a0a222cbcd08d5cb3a8a036baf7959
James Simpson <james.simpson9339@gmail.com>
2018-03-31 16:27:36 -0400

    add interface library helper

---
+++ cmake/interface.cmake
@@ -0,0 +1,28 @@
+
+if(SCAF_INTERFACE_DONE)
+  return()
+endif()
+set(SCAF_INTERFACE_DONE ON)
+
+include(CMakeParseArguments)
+
+macro(add_interface_library target)
+  cmake_parse_arguments(THIS "" "IDE_TARGET" "SOURCES")
+
+  add_library(${target} INTERFACE)
+  set_target_properties(${target} PROPERTIES
+    INTERFACE_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}
+  )
+
+  if (THIS_SOURCES)
+    target_sources(${target} INTERFACE "${THIS_SOURCES}")
+  endif()
+
+  if (THIS_IDE_TARGET)
+    if(THIS_SOURCES)
+      add_custom_target(${THIS_IDE_TARGET} SOURCES "${THIS_SOURCES}")
+    else()
+      message(FATAL_ERROR "Trying to create an IDE target without giving a list of files")
+    endif(THIS_SOURCES)
+  endif()
+endmacro(add_interface_library)
