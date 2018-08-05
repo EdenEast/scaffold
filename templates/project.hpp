f7a0647075baa4fd1588f44a54ce65247a448741
tim <timothee.feuillet@gmail.com>
2018-08-05 16:10:09 -0400

    [scaffold/prig]: Update to the new version of scaffold

--- templates/project.hpp
+++ templates/project.hpp
@@ -1,2 +1,11 @@
 
+// TODO: Insert a header for %%{conf_project_name} here
+
 #pragma once
+
+namespace /*TODO: a namespace name */
+{
+    // TODO: Some code here
+} // namespace neam
+
+// TODO: remove TODOs
